b1184cb2278444fba1d080aee2d359f803796766
thetrueVenom <thetruevenom@gmx.de>
2022-04-12 17:56:21 +0200

    implemented methods to print to a json file and to read json files + updated User Class

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,4 +14,55 @@ public class Main {
 
         System.out.println("Test");
     }
+
+    // saves given Object to a .json file that can be loaded again later
+    // -MF
+    public static void printObjectToJson(Object object, String fileName){
+
+        System.out.println("Saving Object to " + fileName + ".json...");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonToSave = gson.toJson(object);
+
+        try{
+
+            PrintWriter writer = new PrintWriter(fileName + ".json", StandardCharsets.UTF_8);
+            writer.println(jsonToSave);
+            writer.close();
+
+        }catch(Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        System.out.println(fileName + ".json successfully saved!");
+
+    }
+
+    // reads given Object from a .json file to a String value, so it can be loaded into an existing Object
+    // ("pathname" is here the name of the file, because it is saved in the same folder as the program")
+    // -MF
+    public static String readJsonToString(String pathname){
+        String jsonToReturn = "";
+
+        try{
+
+            File fileToRead = new File(pathname + ".json");
+            Scanner reader = new Scanner(fileToRead);
+
+            while(reader.hasNextLine()){
+                jsonToReturn = jsonToReturn + reader.nextLine();
+            }
+
+        }catch(FileNotFoundException e){
+
+            System.out.println(pathname + ".json was not found...");
+            e.printStackTrace();
+
+        }
+
+        return jsonToReturn;
+    }
+
 }
--- User.java
+++ User.java
@@ -1,2 +1,62 @@
 public class User {
+
+    private String name;
+    private int workingHours;
+    private int overHours;
+    private long clockInTime;
+    private long clockOutTime;
+
+    User(){}
+
+    User(String name){
+        this.name = name;
+    }
+
+
+
+
+
+
+
+    // getters and setters below -MF
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setWorkingHours(int workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public int getWorkingHours(){
+        return workingHours;
+    }
+
+    public void setOverHours(int overHours){
+        this.overHours = overHours;
+    }
+
+    public int getOverHours() {
+        return overHours;
+    }
+
+    public long getClockInTime() {
+        return clockInTime;
+    }
+
+    public void setClockInTime(long clockInTime) {
+        this.clockInTime = clockInTime;
+    }
+
+    public long getClockOutTime() {
+        return clockOutTime;
+    }
+
+    public void setClockOutTime(long clockOutTime) {
+        this.clockOutTime = clockOutTime;
+    }
+
 }
